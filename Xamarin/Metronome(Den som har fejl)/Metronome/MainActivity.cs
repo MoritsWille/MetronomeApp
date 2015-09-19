5b434307443a0811b3e9c38b0912817049ec3764
Morits Wille <moritsbw@live.dk>
2015-09-19 22:57:36 +0200

    Icon, Test basic metronom
    Jeg er ret sikker på at fejlen ligger i resource designer filen

---
+++ Xamarin/Metronome(Den som har fejl)/Metronome/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+using Android.App;
+using Android.Media;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MetronomeDemo
+{
+	[Activity (Label = "MetronomeDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		MediaPlayer _player;
+		int BPM;
+		long BeforeTime;
+		Stopwatch StopWatch = new Stopwatch();
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			_player = MediaPlayer.Create(this, Metronome.Resource.Raw.Metronome1a);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Metronome.Resource.Layout.Main);
+			//Get ui elements
+			Android.Widget.Switch StartSwitch = FindViewById<Android.Widget.Switch>(Resource.Id.Switch);
+			EditText BPMText = FindViewById<EditText>(Resource.Id.BPM);
+
+			StartSwitch.CheckedChange += delegate(object sender, CompoundButton.CheckedChangeEventArgs e) {
+				BPM = Convert.ToInt16(BPMText.Text);
+
+				StopWatch.Start();
+				WatchStart:
+				_player.Start();
+				BeforeTime = StopWatch.ElapsedMilliseconds;
+				IfStart:
+				if (BeforeTime + 60000 / BPM == StopWatch.ElapsedMilliseconds) goto WatchStart;
+				else goto IfStart;
+			}
+		}
+	}
+}
